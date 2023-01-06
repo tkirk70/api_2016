ed87183f1c0dd20af07b268dcd89fe0509b584b1
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2023-01-06 11:27:30 -0500

    Adding simple .py file for Streamlit

---
+++ sortable_draft.py
@@ -0,0 +1,16 @@
+# Import dependencies
+import pandas as pd
+import streamlit as st 
+
+# Load df from excel
+df = pd.read_excel('combined_newbees_draft.xlsx', index=False)
+
+# Create header and subheader
+st.title('Sortable Newbees Drafts: 2013-2020')
+st.subheader('Use the dropdowns on the left to sort through different teams or years.')
+
+# Diplay the default dataframe.
+st.dataframe(df)
+
+
+st.write('Where did you go wrong?')
