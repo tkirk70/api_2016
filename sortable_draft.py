95a9ae95ce88aa566dc9d87f3a8dba00287498f0
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2023-01-06 12:15:59 -0500

    Update sortable_draft.py

--- sortable_draft.py
+++ sortable_draft.py
@@ -10,7 +10,7 @@ df = pd.read_excel('combined_newbees_draft.xlsx')
 # Filter by year
 years = df['year'].drop_duplicates()
 make_choice = st.sidebar.selectbox('Select your year:', years)
-filtered_df = df[df["year"].isin(make_choice)]
+filtered_df = df[df.loc['year'] == make_choice]
 st.dataframe(filtered_df)
 
 # Get rid of index
